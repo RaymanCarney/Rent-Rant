28cab49dc32db5994495e9b0ec3d152fb1609efd
RaymanCarney <Raymancarney@gmail.com>
2024-05-24 18:23:28 -0400

    Adding places view with mock data

---
+++ views/places/index.jsx
@@ -0,0 +1,24 @@
+// GET /places
+app.get('/', (req, res) => {
+    let places = []
+    res.render('places/index')
+})
+
+function index(data) {
+    let placesFormatted = data.places.map((place) => {
+        return (
+            <div>
+                <h2>{place.name}</h2>
+                <img src={place.pic} alt={place.name} />
+            </div>
+        )
+    })
+    return (
+        <Def>
+            <main>
+                <h1>PLACES INDEX PAGE</h1>
+                {placesFormatted}
+            </main>
+        </Def>
+    )
+}
