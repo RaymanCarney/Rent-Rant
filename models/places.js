c5ffbb5c8789a64bd64b23f4f78ae4057a2a43cf
RaymanCarney <Raymancarney@gmail.com>
2024-05-24 18:44:17 -0400

    Adding Bootstrap grid to index page

--- models/places.js
+++ models/places.js
@@ -3,11 +3,11 @@ let places = [{
     city: 'Seattle',
     state: 'WA',
     cuisines: 'Thai, Pan-Asian',
-    pic: 'http://placekitten.com/250/250'
+    pic: '/images/h-thai-ml-tables.png'
 }, {
     name: 'Coding Cat Cafe',
     city: 'Phoenix',
     state: 'AZ',
     cuisines: 'Coffee, Bakery',
-    pic: 'http://placekitten.com/250/250'
+    pic: '/images/coffee-cat.jpg'
 }]
