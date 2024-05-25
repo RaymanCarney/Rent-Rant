2a8658f6c51d90007ac6a589a3ef0c1ef68c224d
RaymanCarney <Raymancarney@gmail.com>
2024-05-24 21:21:01 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -18,6 +18,19 @@ router.get('/:id', (req, res) => {
     }
 })
 
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/edit', { place: places[id] })
+    }
+})
+
 router.post('/', (req, res) => {
     if (!req.body.pic) {
         // Default image if one is not provided
