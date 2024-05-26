995edec2e8f49630960f049c418654edd17f112b
RaymanCarney <Raymancarney@gmail.com>
2024-05-26 15:45:46 -0400

    Ending code for part 10

--- controllers/places.js
+++ controllers/places.js
@@ -1,16 +1,27 @@
 const router = require('express').Router()
 const db = require('../models')
 
-router.get('/:id', (req, res) => {
+router.post('/:id/comment', (req, res) => {
+    console.log(req.body)
     db.Place.findById(req.params.id)
-        .then(place => {
-            res.render('places/show', { place })
+    .then(place => {
+        db.Comment.create(req.body)
+        .then(comment => {
+            place.comments.push(comment.id)
+            place.save()
+            .then(() => {
+                res.redirect(`/places/${req.params.id}`)
+            })
         })
         .catch(err => {
-            console.log('err', err)
             res.render('error404')
         })
+    })
+    .catch(err => {
+        res.render('error404')
+    })
 })
+
 router.post('/', (req, res) => {
     if (!req.body.pic) {
         // Default image if one is not provided
