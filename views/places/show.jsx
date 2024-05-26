061bfecfbb6640fe6d0e183f053545ff8829f761
RaymanCarney <Raymancarney@gmail.com>
2024-05-26 16:46:06 -0400

    Code for part 11

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,11 +7,29 @@ function show(data) {
             No comments yet!
         </h3>
     )
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = Math.round(sumRatings / data.place.comments.length)
+        let stars = ''
+        for (let i = 0; i < averageRating; i++) {
+            stars += 'â­ï¸'
+        }
+        rating = (
+            <h3>
+                {stars} stars
+            </h3>
+        )
         comments = data.place.comments.map(c => {
             return (
                 <div className="border">
-                    <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
                     <h4>{c.content}</h4>
                     <h3>
                         <stong>- {c.author}</stong>
@@ -21,15 +39,45 @@ function show(data) {
             )
         })
     }
+
+
     return (
         <Def>
             <main>
                 <div className="row">
-                    ...
+                    <div className="col-sm-6">
+                        <img src={place.pic} alt={place.name} />
+                        <h3>
+                            Located in {place.city}, {place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                        <h2>Rating</h2>
+                        <h3>
+                            Not Rated
+                        </h3>
+                        <h2>
+                            Description
+                        </h2>
+                        <h3>
+                            {place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {place.cuisines}
+                        </h4>
+                        <a href={`/places/${place.id}/edit`} className="btn btn-warning">Edit</a>
+                        <form method="POST" action={`/places/${place.id}?_method=DELETE`}>
+                            <button type="submit" className="btn btn-danger">Delete</button>
+                        </form>
+                    </div>
+                    <div className="col-sm-12">
+                        <h2>
+                            Comments
+                        </h2>
+                        {comments}
+                    </div>
+                    <a href={`/places/${place.id}/comment`}>Add New Comment</a>
                 </div>
-                <hr />
-                <h2>Comments</h2>
-                {comments}
             </main>
         </Def>
     )
